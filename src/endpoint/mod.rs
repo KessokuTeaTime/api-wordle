708983726d17dc9ea65ea29cc1f11318c6a02495
KrLite <wang070621@foxmail.com>
2025-08-13 00:03:27 +0800

    ♻️ Correct routing

--- src/endpoint/mod.rs
+++ src/endpoint/mod.rs
@@ -13,26 +13,12 @@ pub mod internal;
 pub mod root;
 
 /// Routes an [`Router`] with the endpoints defined by this module.
-pub fn route_from(mut app: Router) -> Router {
-    app = route_gets(app);
-    app = route_posts(app);
-    app = route_deletes(app);
-    app.layer(from_fn(log_request))
-}
-
-fn route_gets(app: Router) -> Router {
-    app.route("/", get(root::get))
+pub fn route_from(app: Router) -> Router {
+    app.route("/", get(root::get).post(root::post).delete(root::delete))
         .route("/dates", get(dates::get))
-}
-
-fn route_posts(app: Router) -> Router {
-    app.route(
-        "/internal/update",
-        post(internal::update::post).route_layer(kessoku_private_ci_authorization()),
-    )
-    .route("/", post(root::post))
-}
-
-fn route_deletes(app: Router) -> Router {
-    app.route("/", delete(root::delete))
+        .route(
+            "/internal/update",
+            post(internal::update::post).route_layer(kessoku_private_ci_authorization()),
+        )
+        .layer(from_fn(log_request))
 }
