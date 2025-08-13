8b598463603199954b742f521eeb23089fc817fb
KrLite <68179735+KrLite@users.noreply.github.com>
2025-08-13 11:17:56 +0800

    🐛 Trying to fix 404

--- src/endpoint/auth/mod.rs
+++ src/endpoint/auth/mod.rs
@@ -2,12 +2,13 @@
 
 use axum::{
     http::{StatusCode, header},
+    request::Request,
     response::IntoResponse,
 };
 
 use crate::middleware::auth::generate_paseto_token;
 
-pub async fn get() -> impl IntoResponse {
+pub async fn get(_request: Request) -> impl IntoResponse {
     let token = generate_paseto_token().await;
     (StatusCode::OK, [(header::AUTHORIZATION, token)])
 }
