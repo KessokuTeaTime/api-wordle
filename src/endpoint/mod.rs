808455ad1a860486d75d19f586ddae7d8697b8cc
KrLite <wang070621@foxmail.com>
2025-08-13 11:05:09 +0800

    ♻️ Debug

--- src/endpoint/mod.rs
+++ src/endpoint/mod.rs
@@ -26,10 +26,7 @@ pub fn route_from(app: Router) -> Router {
             .delete(root::delete),
     )
     .route("/dates", get(dates::get))
-    .route(
-        "/auth",
-        get(auth::get).route_layer(admin_password_authorization()),
-    )
+    .route("/auth", get(auth::get))
     .route(
         "/internal/update",
         post(internal::update::post).route_layer(kessoku_private_ci_authorization()),
