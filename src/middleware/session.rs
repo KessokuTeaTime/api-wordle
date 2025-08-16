53ba20f55ba8c0c5b673fc8eec8553d8e6b90e33
KrLite <wang070621@foxmail.com>
2025-08-16 22:19:38 +0800

    ♻️ ???

--- src/middleware/session.rs
+++ src/middleware/session.rs
@@ -46,6 +46,7 @@ pub async fn validate_session_token(
     next: Next,
 ) -> Response {
     info!("validating session token for {addr}…");
+    info!("{:?}", jar);
 
     let token = match jar.get(cookies::SESSION_TOKEN) {
         Some(cookie) => {
