4f5910f84da30eec154c140b2eb51edcce66bd86
KrLite <wang070621@foxmail.com>
2025-08-16 21:07:44 +0800

    ♻️ Refactor response of `/dates`

--- src/endpoint/dates/mod.rs
+++ src/endpoint/dates/mod.rs
@@ -1,17 +1,27 @@
 //! Endpoint `/dates`.
 
+use crate::database::{self, tables::puzzles::get_dates};
+
 use axum::{Json, http::StatusCode, response::IntoResponse};
 use entity::PuzzleDate;
+use serde::Serialize;
 
-use crate::database::{self, tables::puzzles::get_dates};
+#[derive(Debug, Clone, Serialize)]
+pub struct GetResponse {
+    count: usize,
+    dates: Vec<PuzzleDate>,
+}
 
 pub async fn get() -> impl IntoResponse {
     let db = database::acquire_or_response!();
 
-    let dates: Vec<String> = get_dates(&db, false)
-        .await
-        .iter()
-        .map(PuzzleDate::to_string)
-        .collect();
-    (StatusCode::OK, Json(dates)).into_response()
+    let dates: Vec<PuzzleDate> = get_dates(&db, false).await;
+    (
+        StatusCode::OK,
+        Json(GetResponse {
+            count: dates.len(),
+            dates,
+        }),
+    )
+        .into_response()
 }
