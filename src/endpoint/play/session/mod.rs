8937ca93427392d4da1caa94143499abebc16849
KrLite <wang070621@foxmail.com>
2025-08-16 22:13:38 +0800

    🐛 But why?

--- src/endpoint/play/session/mod.rs
+++ src/endpoint/play/session/mod.rs
@@ -10,16 +10,18 @@ use axum_extra::extract::{CookieJar, cookie::Cookie};
 
 pub async fn get(jar: CookieJar, session: Option<Extension<SessionToken>>) -> impl IntoResponse {
     match session {
-        Some(Extension(SessionToken(session))) => {
-            let mut cookie = Cookie::new(cookies::SESSION_TOKEN, session);
-            cookie.set_path("/play");
-            (StatusCode::OK, jar.add(cookie)).into_response()
-        }
+        Some(Extension(SessionToken(session))) => (
+            StatusCode::OK,
+            jar.add(Cookie::new(cookies::SESSION_TOKEN, session)),
+        )
+            .into_response(),
         None => {
             let token = generate_session_token().await;
-            let mut cookie = Cookie::new(cookies::SESSION_TOKEN, token);
-            cookie.set_path("/play");
-            (StatusCode::CREATED, jar.add(cookie)).into_response()
+            (
+                StatusCode::CREATED,
+                jar.add(Cookie::new(cookies::SESSION_TOKEN, token)),
+            )
+                .into_response()
         }
     }
 }
