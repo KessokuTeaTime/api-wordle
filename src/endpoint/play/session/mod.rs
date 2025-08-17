5be688eaadc8f538ef00f513d07091223482fa4b
KrLite <wang070621@foxmail.com>
2025-08-17 15:29:36 +0800

    Update mod.rs

--- src/endpoint/play/session/mod.rs
+++ src/endpoint/play/session/mod.rs
@@ -22,8 +22,9 @@ use serde::Deserialize;
 pub async fn get(jar: CookieJar, session: Option<Extension<SessionToken>>) -> impl IntoResponse {
     fn setup_cookie(session: String) -> Cookie<'static> {
         let mut cookie = Cookie::new(cookies::SESSION_TOKEN, session);
+        cookie.set_http_only(true);
         cookie.set_same_site(SameSite::None);
-        cookie.set_secure(false);
+        cookie.set_secure(true);
         cookie
     }
 
