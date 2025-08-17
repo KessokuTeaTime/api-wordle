c0b1287a3d41bbde14dea5d390e87cde135b0f9e
KrLite <wang070621@foxmail.com>
2025-08-17 14:19:03 +0800

    🐛 Fix cookie setup

--- src/endpoint/play/session/mod.rs
+++ src/endpoint/play/session/mod.rs
@@ -9,19 +9,21 @@ use axum::{Extension, http::StatusCode, response::IntoResponse};
 use axum_extra::extract::{CookieJar, cookie::Cookie};
 
 pub async fn get(jar: CookieJar, session: Option<Extension<SessionToken>>) -> impl IntoResponse {
+    fn setup_cookie(session: String) -> Cookie<'static> {
+        let mut cookie = Cookie::new(cookies::SESSION_TOKEN, session);
+        cookie.set_http_only(true);
+        cookie.set_same_site(None);
+        cookie.set_secure(true);
+        cookie
+    }
+
     match session {
-        Some(Extension(SessionToken(session))) => (
-            StatusCode::OK,
-            jar.add(Cookie::new(cookies::SESSION_TOKEN, session)),
-        )
-            .into_response(),
+        Some(Extension(SessionToken(session))) => {
+            (StatusCode::OK, jar.add(setup_cookie(session))).into_response()
+        }
         None => {
             let token = generate_session_token().await;
-            (
-                StatusCode::CREATED,
-                jar.add(Cookie::new(cookies::SESSION_TOKEN, token)),
-            )
-                .into_response()
+            (StatusCode::CREATED, jar.add(setup_cookie(token))).into_response()
         }
     }
 }
