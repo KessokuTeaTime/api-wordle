b55d572d2dc1a91efec38e20b61333b9b2f16d4f
KrLite <wang070621@foxmail.com>
2025-08-17 14:54:14 +0800

    🐛 Maybe fix cookie on Safari
    Fuck you Safari

--- src/endpoint/play/session/mod.rs
+++ src/endpoint/play/session/mod.rs
@@ -1,32 +1,55 @@
 //! Endpoint `/play/session`.
 
+use std::net::SocketAddr;
+
 use crate::{
     cookies,
     middleware::session::{SessionToken, generate_session_token},
 };
 
-use axum::{Extension, http::StatusCode, response::IntoResponse};
+use axum::{
+    Extension,
+    extract::{ConnectInfo, Query},
+    http::StatusCode,
+    response::IntoResponse,
+};
 use axum_extra::extract::{
     CookieJar,
     cookie::{Cookie, SameSite},
 };
+use serde::Deserialize;
 
-pub async fn get(jar: CookieJar, session: Option<Extension<SessionToken>>) -> impl IntoResponse {
-    fn setup_cookie(session: String) -> Cookie<'static> {
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetParams {
+    dev: Option<bool>,
+}
+
+pub async fn get(
+    jar: CookieJar,
+    Query(params): Query<GetParams>,
+    session: Option<Extension<SessionToken>>,
+) -> impl IntoResponse {
+    fn setup_cookie(session: String, secure: bool) -> Cookie<'static> {
         let mut cookie = Cookie::new(cookies::SESSION_TOKEN, session);
         cookie.set_http_only(true);
-        cookie.set_same_site(Some(SameSite::None));
-        cookie.set_secure(true);
+        cookie.set_same_site(SameSite::None);
+        cookie.set_secure(secure);
         cookie
     }
 
     match session {
-        Some(Extension(SessionToken(session))) => {
-            (StatusCode::OK, jar.add(setup_cookie(session))).into_response()
-        }
+        Some(Extension(SessionToken(session))) => (
+            StatusCode::OK,
+            jar.add(setup_cookie(session, !params.dev.unwrap_or(false))),
+        )
+            .into_response(),
         None => {
             let token = generate_session_token().await;
-            (StatusCode::CREATED, jar.add(setup_cookie(token))).into_response()
+            (
+                StatusCode::CREATED,
+                jar.add(setup_cookie(token, !params.dev.unwrap_or(false))),
+            )
+                .into_response()
         }
     }
 }
