7b2ca29488c63f43cfb500169773eeb1940ba275
KrLite <wang070621@foxmail.com>
2025-08-18 14:43:44 +0800

    🐛 Fix letter count not set

--- src/endpoint/play/start/mod.rs
+++ src/endpoint/play/start/mod.rs
@@ -14,7 +14,7 @@ use crate::{
 
 use axum::{Extension, Json, extract::Query, http::StatusCode, response::IntoResponse};
 use entity::{
-    HISTORY_MAX_TRIES, PuzzleDate, PuzzleSolution, SubmitHistory, SubmitWord,
+    HISTORY_MAX_TRIES, PUZZLE_LETTERS_COUNT, PuzzleDate, PuzzleSolution, SubmitHistory, SubmitWord,
     puzzles::Model as Puzzle,
 };
 use serde::{Deserialize, Serialize};
@@ -96,6 +96,7 @@ pub async fn get(
                 Ok(_) => (
                     StatusCode::CREATED,
                     Json(GetResponse {
+                        letters_count: PUZZLE_LETTERS_COUNT,
                         remaining_tries: HISTORY_MAX_TRIES,
                         is_dirty: false,
                         is_completed: false,
