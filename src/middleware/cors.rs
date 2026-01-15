1ab4515299d5f5e09199cb2df1c76ff7e7172819
KrLite <wang070621@foxmail.com>
2026-01-15 13:05:39 +0800

    fix: this is actually fine

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -13,13 +13,10 @@ pub mod layers {
     static_lazy_lock! {
         /// The layer to handle Cross-Origin Resource Sharing (CORS).
         pub CORS: CorsLayer = CorsLayer::new()
-        .allow_origin(AllowOrigin::async_predicate(|origin: HeaderValue, request_parts: &request::Parts| {
-            let headers = request_parts.headers.clone();
+        .allow_origin(AllowOrigin::async_predicate(|origin: HeaderValue, _request_parts: &request::Parts| {
             async move {
-                tracing::trace!("CORS async_predicate called with origin: {:?}", origin);
-                tracing::trace!("Request headers: {:?}", headers);
                 let config = CorsConfig::read().unwrap_or_default();
-                tracing::trace!("CORS origin check: {:?}", origin);
+                tracing::trace!("origin: {origin:?}, allowed CORS origins: {:?}", config.origins);
                 config.contains(&origin)
             }
         }))
